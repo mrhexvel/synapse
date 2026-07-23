c787b795f827f72de810a65ec0191eb9896e9981
mrhexvel <mrhexvel@gmail.com>
2026-07-23 07:41:19 +0200

    19: node variables

--- src/features/executions/components/http-request/dialog.tsx
+++ src/features/executions/components/http-request/dialog.tsx
@@ -16,6 +16,7 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
@@ -32,6 +33,13 @@ import { useForm } from "react-hook-form";
 import z from "zod";
 
 const formSchema = z.object({
+  variableName: z
+    .string()
+    .min(1, { error: "Variable name is required" })
+    .regex(/^[A-Za-z_$][A-Za-z0-9_$]*$/, {
+      error:
+        "Variable name must start with a letter or underscore and container only letters, numbers, and underscores",
+    }),
   endpoint: z.url({ error: "Please enter a valid URL" }),
   method: z.enum(["GET", "POST", "PUT", "PATCH", "DELETE"]),
   body: z.string().optional(),
@@ -56,6 +64,7 @@ export const HttpRequestDialog = ({
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      variableName: defaultValues.variableName || "",
       endpoint: defaultValues.endpoint || "",
       method: defaultValues.method || "GET",
       body: defaultValues.body || "",
@@ -65,6 +74,7 @@ export const HttpRequestDialog = ({
   useEffect(() => {
     if (open) {
       form.reset({
+        variableName: defaultValues.variableName || "",
         endpoint: defaultValues.endpoint || "",
         method: defaultValues.method || "GET",
         body: defaultValues.body || "",
@@ -72,6 +82,7 @@ export const HttpRequestDialog = ({
     }
   }, [open, defaultValues, form]);
 
+  const watchVariableName = form.watch("variableName") || "myApiCall";
   const watchMethod = form.watch("method");
   const showBodyField = ["POST", "PUT", "PATCH"].includes(watchMethod);
 
@@ -94,6 +105,23 @@ export const HttpRequestDialog = ({
             onSubmit={form.handleSubmit(handleSubmit)}
             className="space-y-8 mt-4"
           >
+            <FormField
+              control={form.control}
+              name="variableName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Variable Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="myApiCall" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Use this name to reference the result in other nodes:{" "}
+                    {`{{${watchVariableName}.httpResponse.data}}`}
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="method"
@@ -120,6 +148,7 @@ export const HttpRequestDialog = ({
                   <FormDescription>
                     The HTTP method to use for this request
                   </FormDescription>
+                  <FormMessage />
                 </FormItem>
               )}
             />
@@ -139,6 +168,7 @@ export const HttpRequestDialog = ({
                     Static URL or use {"{{variables}}"} for simple values or{" "}
                     {"{{json variables}}"} ro stringify objects
                   </FormDescription>
+                  <FormMessage />
                 </FormItem>
               )}
             />
@@ -151,6 +181,7 @@ export const HttpRequestDialog = ({
                     <FormLabel>Request Body</FormLabel>
                     <FormControl>
                       <Textarea
+                        {...field}
                         placeholder={
                           '{\n "userId": "{{httpResponse.data.id}}",\n "name": "{{httpResponse.data.name}}",\n "items": "{{httpResponse.data.items}}"\n}'
                         }
@@ -162,6 +193,7 @@ export const HttpRequestDialog = ({
                       simple values or {"{{json variables}}"} to stringify
                       objects
                     </FormDescription>
+                    <FormMessage />
                   </FormItem>
                 )}
               />
--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -7,6 +7,7 @@ import { BaseExecutionNode } from "../base-execution-node";
 import { HttpRequestDialog, type HttpRequestFormValues } from "./dialog";
 
 type HttpRequestNodeData = {
+  variableName?: string;
   endpoint?: string;
   method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
   body?: string;
@@ -43,7 +44,7 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
     ? `${nodeData.method || "GET"}: ${nodeData.endpoint}`
     : "Not configured";
 
-  const nodeStatus = "success";
+  const nodeStatus = "initial";
 
   return (
     <>
